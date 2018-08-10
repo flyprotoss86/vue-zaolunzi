df9f974baa771497957dae9c6d865b2ec99c04f2
flyprotoss86 <flyprotoss@gmail.com>
2018-08-10 19:34:11 +0800

    install parcel

--- src/Button.vue
+++ src/Button.vue
@@ -1,15 +1,35 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`${iconPosition}`]: true}">
+        <svg v-if="icon" class="icon"><use :xlink:href="`#icon-${icon}`"></use></svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
     export default {
-        //name: "Button"
+        name: "Button",
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator(value){
+                    return value === 'left' || value === 'right'
+                }
+            }
+        }
+            //['icon', 'iconPosition']
     }
 </script>
 
 <style lang="scss">
     .g-button{
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: top;
         font-size: var(--font-size);
         height: var(--button-height);
         padding: 0 1em;
@@ -24,6 +44,20 @@
         }
         &:focus{
             outline: none;
+        }
+        &.left{
+            .icon{
+                margin-right: 0.3em;
+            }
+        }
+        &.right{
+            .content{
+                order: 1
+            }
+            .icon{
+                order: 2;
+                margin-left: 0.3em;
+            }
         }
     }
 </style>
